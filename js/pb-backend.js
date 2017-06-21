65431e961508645a8f860383f85d98f2058c4ba8
Kelden Lin <keldenlin@gmail.com>
2017-06-20 23:14:45 -0700

    Replaced images, fixed unicode issues

--- js/pb-backend.js
+++ js/pb-backend.js
@@ -12,7 +12,7 @@ window.onload = function() {
       var reader = new FileReader();
       reader.readAsDataURL(request.response);
       reader.onload =  function(e){
-          plainData = decode64(e.target.result.split(",")[1]);
+          plainData = b64DecodeUnicode(e.target.result.split(",")[1]);
           var processed = teamProcess(plainData);
           var teams = processed[0];
           var playerData = processed[1];
@@ -144,50 +144,8 @@ function playerbaseStyling(teamLen) {
 
 
 // Decoder
-var keyStr = "ABCDEFGHIJKLMNOP" +
-               "QRSTUVWXYZabcdef" +
-               "ghijklmnopqrstuv" +
-               "wxyz0123456789+/" +
-               "=";
-
-function decode64(input) {
-   var output = "";
-   var chr1, chr2, chr3 = "";
-   var enc1, enc2, enc3, enc4 = "";
-   var i = 0;
-
-   // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
-   var base64test = /[^A-Za-z0-9\+\/\=]/g;
-   if (base64test.exec(input)) {
-      alert("There were invalid base64 characters in the input text.\n" +
-            "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
-            "Expect errors in decoding.");
-   }
-   input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-
-   do {
-      enc1 = keyStr.indexOf(input.charAt(i++));
-      enc2 = keyStr.indexOf(input.charAt(i++));
-      enc3 = keyStr.indexOf(input.charAt(i++));
-      enc4 = keyStr.indexOf(input.charAt(i++));
-
-      chr1 = (enc1 << 2) | (enc2 >> 4);
-      chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-      chr3 = ((enc3 & 3) << 6) | enc4;
-
-      output = output + String.fromCharCode(chr1);
-
-      if (enc3 != 64) {
-         output = output + String.fromCharCode(chr2);
-      }
-      if (enc4 != 64) {
-         output = output + String.fromCharCode(chr3);
-      }
-
-      chr1 = chr2 = chr3 = "";
-      enc1 = enc2 = enc3 = enc4 = "";
-
-   } while (i < input.length);
-
-   return unescape(output);
+function b64DecodeUnicode(str) {
+   return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
+       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+   }).join(''));
 }
